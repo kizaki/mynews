0ea857177911d975c8056ed655c67cbb254b4e23
kizaki <kizaki.aiit@gmail.com>
2020-09-12 22:36:18 +0000

    Bladeテンプレートの継承とLaravel Mixの使い方を活用してみよう

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,17 @@
-<!Doctype html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Cmpatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, intial-scale=1">
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>プロフィール作成画面</h1>
-    </body>
-</html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
